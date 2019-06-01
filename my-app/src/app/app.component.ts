86803ea9996f2358534dc790b4b55784ef278103
lelani82 <lelaniparker@hotmail.com>
2019-06-01 15:48:18 +1000

    push before break

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -11,4 +11,12 @@ export class AppComponent {
   addTodo(value){    
     this.todoArray.push(value);    
     console.log(this.todoArray)  } 
+    /*delete item*/  deleteItem(todo){   
+      for(let i=0 ;i<= this.todoArray.length ;i++){    
+        if(todo== this.todoArray[i]){     
+          this.todoArray.splice(i,1)    }   }  }
+  // submit Form  
+  todoSubmit(value:any){     if(value!==""){    this.todoArray.push(value.todo)     
+    this.todoForm.reset()    }else{      alert('Field required **')    }      }
 };
+
